a3140d4da5b48aafa7610318c14cd124bfe87f91
JISOO PARK <jso9603@gmail.com>
2019-07-01 15:10:32 +0900

    서명 라이브러리 테스트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import SignatureCanvas from "react-signature-canvas";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signature: ''
+    }
+
+    this.sigCanvas = React.createRef();
+  }
+
+  saveSignature = () => {
+    console.log(this.sigCanvas.toDataURL());
+    this.setState({ signature: this.sigCanvas.toDataURL() });
+  };
+
+  againgSignature = () => {
+    this.sigCanvas.clear();
+    this.setState({ signature: '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Signature</h1>
+        <div style={{ backgroundColor: '#f1f1f1' }}>
+          <span className="description">서명을 해주세요.</span>
+          <SignatureCanvas
+            ref={(ref) => { this.sigCanvas = ref }}
+            canvasProps={{width: 375, height: 200, className: 'sigCanvas'}}
+          />
+        </div>
+        <button
+          className="ok"
+          onClick={this.saveSignature}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          확인
+        </button>
+        <br/>
+        <button
+          className="reset"
+          onClick={this.againgSignature}
+        >
+          다시쓰기
+        </button>
+        <br/><br/><br/>
+
+        <div>
+          <h1>Result</h1>
+          <img src={this.state.signature} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
